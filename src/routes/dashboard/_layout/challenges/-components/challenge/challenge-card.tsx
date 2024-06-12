55dce6d9ae898ec05ed418693fdb6b1ba2d213d0
haruBlank00 <harublank00@gmail.com>
2024-06-12 16:38:41 +0545

    add hover to card

--- src/routes/dashboard/_layout/challenges/-components/challenge/challenge-card.tsx
+++ src/routes/dashboard/_layout/challenges/-components/challenge/challenge-card.tsx
@@ -7,7 +7,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Challenge } from "../../-interface";
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import { Eye, Pencil } from "lucide-react";
 import { DeleteChallengeAlert } from "../delete-modal/delete-modal";
 import { Separator } from "@/components/ui/separator";
@@ -17,41 +17,46 @@ type ChallengeCardProps = {
 };
 
 export const ChallengeCard = ({ challenge }: ChallengeCardProps) => {
+  const navigate = useNavigate()
   const challengeDays = `${challenge.days} day${challenge.days > 1 ? "s" : ""}`;
-  return (
-    <Link
-      to="/dashboard/challenges/$challengeId/logs"
-      params={{
+
+  const redirectToLogPage = () => {
+    navigate({
+      to: "/dashboard/challenges/$challengeId/logs",
+      params: {
         challengeId: challenge._id,
-      }}
-    >
-      <Card className="cursor-pointer group h-full border-primary">
-        <CardHeader className="flex flex-row justify-between">
-          <div>
-            <CardTitle className="capitalize text-primary mb-2">{challenge.name}</CardTitle>
-            <CardDescription>{challengeDays}</CardDescription>
-          </div>
+      }
 
-          <div className="flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity items-center">
-            <Link
-              to="/dashboard/challenges/$challengeId"
-              params={{
-                challengeId: challenge._id,
-              }}
-            >
-              <Pencil className="text-green-600" />
-            </Link>
-            <Eye className="text-blue-600" />
-            <DeleteChallengeAlert id={challenge._id} />
-          </div>
-        </CardHeader>
+    })
+  }
 
-        <Separator className="bg-primary w-[90%] mx-auto" />
+  return (
+    <Card className="cursor-pointer group h-full border-primary shadow-md hover:-translate-y-1 transition-transform" onClick={redirectToLogPage}>
+      <CardHeader className="flex flex-row justify-between">
+        <div>
+          <CardTitle className="capitalize text-primary mb-2">{challenge.name}</CardTitle>
+          <CardDescription>{challengeDays}</CardDescription>
+        </div>
 
-        <CardContent className="text-white p-0">
-          <MdxPreview markdown={challenge.description} />
-        </CardContent>
-      </Card>
-    </Link>
+        <div className="flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity items-center">
+          <Link
+            to="/dashboard/challenges/$challengeId"
+            params={{
+              challengeId: challenge._id,
+            }}
+          >
+            <Pencil className="text-green-600" />
+          </Link>
+          <Eye className="text-blue-600" />
+          <DeleteChallengeAlert id={challenge._id} />
+        </div>
+      </CardHeader>
+
+      <Separator className="bg-primary w-[90%] mx-auto" />
+
+      <CardContent className="text-white p-0">
+        <MdxPreview markdown={challenge.description} />
+      </CardContent>
+    </Card>
   );
 };
